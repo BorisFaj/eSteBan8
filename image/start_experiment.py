3574a13eb7ecf7a9d2cfc34ea91a045ba095d5f2
BorisFaj <boris88ab@gmail.com>
2025-04-24 21:16:53 +0200

    mlflow fix

--- image/start_experiment.py
+++ image/start_experiment.py
@@ -2,23 +2,25 @@ import mlflow
 from dotenv import load_dotenv
 import os
 
-load_dotenv()
 
 def start_mlflow(params: dict, run_name: str):
-
+    load_dotenv()
     mlflow.set_tracking_uri(os.getenv("DATABRICKS_HOST"))
+
     os.environ["MLFLOW_TRACKING_USERNAME"] = os.getenv("DATABRICKS_USER_NAME")
     os.environ["MLFLOW_TRACKING_PASSWORD"] = os.getenv("DATABRICKS_TOKEN")
 
+    databricks_user = os.getenv("DATABRICKS_USER_NAME")
+    experiment_name = os.getenv("EXPERIMENT_NAME")
+
+    # Ruta completa del experimento
+    experiment_path = f"/Users/{databricks_user}/{experiment_name}"
+
     print("MLflow Tracking URI:", mlflow.get_tracking_uri())
-    client = mlflow.tracking.MlflowClient()
-    print("Experimentos disponibles:", client.list_experiments())
+    print("Usando experimento:", experiment_path)
 
-    _params = {}
-
-
-    mlflow.set_experiment(run_name)  # opcional, si quieres organizar por run
-    mlflow.start_run()
+    mlflow.set_experiment(experiment_path)
+    mlflow.start_run(run_name=run_name)
     mlflow.log_params(params)
 
     return mlflow
