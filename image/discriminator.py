c1102801dbdada9104592af639b7f42fcce50ce4
BorisFaj <boris88ab@gmail.com>
2025-05-02 16:40:53 +0200

    power up

--- image/discriminator.py
+++ image/discriminator.py
@@ -1,5 +1,6 @@
 import torch
 import torch.nn as nn
+import torch.nn.init as init
 
 class Discriminator(nn.Module):
     def __init__(self, image_channels):
@@ -12,6 +13,17 @@ class Discriminator(nn.Module):
         )
         self.fc = None  # capa lineal definida en el primer forward
 
+        # Inicialización de pesos
+        self._init_weights()
+
+    def _init_weights(self):
+        for m in self.modules():
+            if isinstance(m, nn.Conv2d):
+                # Kaiming para activaciones tipo ReLU/LeakyReLU
+                init.kaiming_normal_(m.weight, nonlinearity='leaky_relu')
+                if m.bias is not None:
+                    init.constant_(m.bias, 0)
+
     def forward(self, image):
         x = self.conv(image)
         B, C, H, W = x.shape
@@ -19,5 +31,7 @@ class Discriminator(nn.Module):
 
         if self.fc is None:
             self.fc = nn.Linear(C * H * W, 1).to(x.device).half()
+            init.kaiming_normal_(self.fc.weight, nonlinearity='leaky_relu')
+            init.constant_(self.fc.bias, 0)
 
         return torch.sigmoid(self.fc(x))
